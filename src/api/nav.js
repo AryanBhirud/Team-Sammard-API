587955b6d17a27e6e0c0249fdecbb3d25d32402a
Aryan <aryan.bhirud@hotmail.com>
2024-09-07 23:21:01 +0530

    Fix: dir issue

--- src/api/nav.js
+++ src/api/nav.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require('path');
+
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join('nav.html'));
+    res.sendFile(path.join(__dirname, 'nav.html'));
 });
 
 module.exports =router;
